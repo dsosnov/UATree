c820b8f82d2d2af910a859621c25a69757854fab
Dmitry Sosnov <dmitry.sosnov@cern.ch>
2018-02-13 15:28:43 +0100

    Disabled storing jets

--- UABaseTree/python/UABaseTree_forward_options_all.py
+++ UABaseTree/python/UABaseTree_forward_options_all.py
@@ -4,7 +4,7 @@
 doDAvertex   = False		#if you want to enable the AnnealingVertexing + stores it
 doMBTracking = False		#does the low-pt tracking + merging with generalTracks + vertices + stores them. If false, store only generalTracks & offlinePrimaryVertices.
 useMITFilter = False		#Not the official CMS one, but cleans better. Needed for low-pt tracking. If off, no filter is used. //the standard "noscraping" filter is used.
-storeJets    = True		#stores jets. Need to choose which collections and corrections to store, and if do the Dijets.
+storeJets    = False		#stores jets. Need to choose which collections and corrections to store, and if do the Dijets.
 storeMET     = True		#stores "met", "pfMet" and "tcMet". If isMonteCarlo=true , also stores "genMetTrue".
 storeCastor  = False 		#stores castorrechits , castorjets
 keepCMSData  = False 		#make another CMSSW root files with all the collections from the input file and those created in the path
